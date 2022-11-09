5efaf4523cf3afdd71c8e210e2ccfa868bcdcdf0
MarcD86 <marc_duim@live.nl>
2022-11-09 08:44:50 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+// Async - await Full data
+async function fetchData() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        console.log(result)
+    } catch(error) {
+        console.error(error);
+    }
+}
+fetchData();
